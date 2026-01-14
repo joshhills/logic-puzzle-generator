cbb9d0318f8e781a9cb7a81689ade4fddfa006af
Josh Hills <joshuamarcushills@gmail.com>
2026-01-14 02:23:42 +0000

    chore: implement dynamic versioning using vite define

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,8 +3,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+import fs from 'fs'
+
+const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'))
+
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    '__APP_VERSION__': JSON.stringify(packageJson.version),
+  },
   plugins: [react()],
   resolve: {
     alias: {
